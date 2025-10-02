a3eeb5e24ded8cef2e5bc7f2632e92ea1e0dfe35
Amit Giri <120805545+Amit123-ctrl@users.noreply.github.com>
2025-10-02 10:38:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,5 @@ def correct_text():
         return jsonify({"error": "An error occurred while processing the request."}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
